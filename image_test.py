52faa3c957bb720f27b02e641229c0e8c0dc7f26
asakinory <asakinory@gmail.com>
2023-01-03 10:52:30 +0200

    try with no yml

--- image_test.py
+++ image_test.py
@@ -1,8 +1,12 @@
+import random
 def start(args, hkubeapi):
     print('algorithm: range start')
     input = args['input'][0]
-    array = list(range(input))
+    array = [random.random() for _ in range(input)]
     return array
 
+# temp_args_dict = {
+#     'input': [8]
+# }
 
-
+# print(start(temp_args_dict, None))
