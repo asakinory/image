92086c2d2714809b67a4504392d0bea714268da5
asakinory <asakinory@gmail.com>
2023-01-19 15:05:58 +0200

    long image

---
+++ long_image.py
@@ -0,0 +1,6 @@
+import random
+def start(args, hkubeapi):
+    print('algorithm: range start')
+    input = args['input'][0]
+    array = [random.random()**446656556.4343443 for _ in range(input)]
+    return array
